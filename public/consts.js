f8fbc74b6c4dc10fc20a4bbd84fb78031d531247
avishayhajbi <avishay.h@sscisrael.com>
2022-10-13 20:00:40 +0300

    init

--- public/consts.js
+++ public/consts.js
@@ -1,1 +1,1 @@
-var APP_URI = window.location.protocol + "//" + window.location.hostname + (window.location.port ? `:${3001}` : '');
+const APP_URI = window.location.protocol + "//" + window.location.hostname + (window.location.port ? `:${3001}` : '');
