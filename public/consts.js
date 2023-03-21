ed56d9c27da10c7db6781a93ca907f71d376292a
avishayhajbi <avishay@konnecto.com>
2023-03-21 23:37:44 +0200

    adding price and navigation fixes

--- public/consts.js
+++ public/consts.js
@@ -1,2 +1,2 @@
-//const APP_URI = window.location.protocol + "//" + window.location.hostname + (window.location.port ? `:${3001}` : '');
+// const APP_URI = window.location.protocol + "//" + window.location.hostname + (window.location.port ? `:${3001}` : '');
 const APP_URI ='https://toraletter.herokuapp.com'
